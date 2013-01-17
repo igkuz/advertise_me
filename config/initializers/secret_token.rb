b5ce70c10d66a518ad70a97137249eaa7571e6ff
Igor Kuznetsov <igkuznetsov@gmail.com>
2013-01-17 22:59:44 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvertiseMe::Application.config.secret_token = '04da90801a9f770db08c72e4b072b3a39011e0a3c568e4e35e86952ffb5b9be560a007b85429bfe83e4611d114cc3991a06eaecbeee54772a25ade2488a7affb'
